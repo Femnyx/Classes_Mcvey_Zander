de586450886d0d82e431366a0948d2e061f6981b
Zander <114107976+Femnyx@users.noreply.github.com>
2023-04-24 14:39:07 -0500

    Classes here it is!

---
+++ Classes_Mcvey_Zander/Greetings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Mcvey_Zander
+{
+    class Greetings
+    {
+        /// <summary>
+        /// It prints text that greets the user.
+        /// </summary>
+        public void Welcome()
+        {
+            Console.WriteLine("Hello Traveler!, welcome!");
+        }
+
+        /// <summary>
+        /// It prints text that thanks the user.
+        /// </summary>
+        /// <param name="name"></param>
+        public void Hello(string name)
+        {
+            Console.WriteLine($"{name}, thank you for joining us today!");
+        }
+
+    }
+}
--- Classes_Mcvey_Zander/Program.cs
+++ Classes_Mcvey_Zander/Program.cs
@@ -6,7 +6,18 @@ namespace Classes_Mcvey_Zander
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           //This links both classes together to use them both into one program.
+            Greetings greetings = new Greetings(); 
+          
+            //This command greets the user using the other class in this program.
+            greetings.Welcome();
+
+            //The user inserts their name into the command.
+            Console.WriteLine("What is your name squire?");
+                string userName = Console.ReadLine();
+
+            //The command greats the user with userName. 
+            greetings.Hello(userName);
         }
     }
 }
